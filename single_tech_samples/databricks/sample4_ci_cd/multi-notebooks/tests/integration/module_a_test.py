a68217bc9b8c62d737ac98aab1334b0ac21573f9
Bo Wang <bwa@microsoft.com>
2021-05-19 23:20:18 +0800

    test module name change

--- single_tech_samples/databricks/sample4_ci_cd/multi-notebooks/tests/integration/module_a_test.py
+++ single_tech_samples/databricks/sample4_ci_cd/multi-notebooks/tests/integration/module_a_test.py
@@ -1,15 +1,15 @@
 from runtime.nutterfixture import NutterFixture
 
-class TestMainNotebook(NutterFixture):
-    def run_test_main(self):
+class TestmoduleANotebook(NutterFixture):
+    def run_test_module_a(self):
       print("test_name_runing")
 #       dbutils.notebook.run('../../notebooks/main_notebook.py', 600)
 
-    def assertion_test_main(self):
+    def assertion_test_module_a(self):
       print("assert_test_name_runing")
       assert (1 == 1)
       
-result = TestMainNotebook().execute_tests()
+result = TestmoduleANotebook().execute_tests()
 print(result.to_string())
 # Comment out the next line (result.exit(dbutils)) to see the test result report from within the notebook
 result.exit(dbutils)
